671f852f9bc6cc5cf33d39cc813f04eae2be9c6c
FRANCISCO-code-max <126724839+FRANCISCO-code-max@users.noreply.github.com>
2023-03-01 23:38:57 +0100

    javadoc

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Cuentas","l":"CCuenta"},{"p":"Cuentas","l":"EntornosDesarrollo"},{"p":"Cuentas","l":"EntornosDesarrollo.Main"}];updateSearchResults();
